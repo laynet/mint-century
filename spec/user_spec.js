348e6d00c776719f71ffae3012cd90c6bd2479d3
Layne <layneingramtaylor@gmail.com>
2019-12-29 17:31:38 -0600

    jasmine error

---
+++ spec/user_spec.js
@@ -0,0 +1,73 @@
+const sequelize = require("../../src/db/models/index").sequelize;
+const User = require("../../src/db/models").User;
+describe("User", () => {
+  beforeEach(done => {
+    sequelize
+      .sync({ force: true })
+      .then(() => {
+        done();
+      })
+      .catch(err => {
+        console.log(err);
+        done();
+      });
+  });
+
+  describe("#create()", () => {
+    it("should create a User object with a valid email and password", done => {
+      URLSearchParams.create({
+        email: "user@example.com",
+        password: "123456"
+      })
+        .then(user => {
+          expect(user.email).toBe("user@example.com");
+          expect(user.id).toBe(1);
+          done();
+        })
+        .catch(err => {
+          console.log(err);
+          done();
+        });
+    });
+
+    it("should not create a user with invalid email or password", done => {
+      User.create({
+        email: "not-an-email",
+        password: "123"
+      })
+        .then(user => {
+          done();
+        })
+        .catch(err => {
+          expect(err.message).toContain(
+            "Validation error: must be a valid email"
+          );
+          done();
+        });
+    });
+    it("should not create a user with an email already taken", done => {
+      User.create({
+        email: "user@example.com",
+        password: "123456"
+      })
+        .then(user => {
+          User.create({
+            email: "user@example.com",
+            password: "sodnvslajnvklasdjnklasjndv"
+          })
+            .then(user => {
+              done();
+            })
+            .catch(err => {
+              expect(err.message).toContain("Validation error");
+              done();
+            });
+          done();
+        })
+        .catch(err => {
+          console.log(err);
+          done;
+        });
+    });
+  });
+});
